0b5c7b325ac04ff7b36c20a4d015184bf3a5b952
harrywardy-cmd <harrywardy303@gmail.com>
2025-03-19 20:26:23 +1100

    defining the move and init

--- snake_game.py
+++ snake_game.py
@@ -8,8 +8,19 @@ BLACK, WHITE, GREEN, RED = (0, 0, 0), (255, 255, 255), (0, 255, 0), (255, 0, 0)
 
 class Snake:
     def __init__(self):
+        self.body = [(WIDTH // 2, HEIGHT // 2)]
+        self.direction = (GRID_SIZE, 0)
+        self.grow = False
     
-    def move():
+    def move(self):
+        head_x, head_y = self.body[0]
+        new_head = (head_x + self.direction[0], head_y + self.direction[1])
+        
+        if not self.grow:
+            self.body.pop()
+        self.grow = False
+        
+        self.body.insert(0, new_head)
     
     def chance_direction():
     
