126d1f897940d36f59817965a771864283327e7b
harrywardy-cmd <harrywardy303@gmail.com>
2025-03-19 20:21:17 +1100

    defining snake class

--- snake_game.py
+++ snake_game.py
@@ -1,2 +1,28 @@
 import pygame
 import random
+
+# Constants
+WIDTH, HEIGHT = 600, 400
+GRID_SIZE = 20
+BLACK, WHITE, GREEN, RED = (0, 0, 0), (255, 255, 255), (0, 255, 0), (255, 0, 0)
+
+class Snake:
+    def __init__(self):
+    
+    def move():
+    
+    def chance_direction():
+    
+    def snake_grow():
+    
+    def check_collsion(self):
+
+
+class food(self):
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
